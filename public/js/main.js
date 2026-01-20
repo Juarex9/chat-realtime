0abae4d478ca1c8911e2fd86732fad97bb3a929b
juarex9 <agustinjuarez375@gmail.com>
2026-01-20 13:37:06 -0300

    fix: resolve problems to send message

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,37 @@
 const socket = io();
 let user;
-const userName = document.querySelector("#userName"),
-    messages = document.querySelector("#messages"),
-    messageInput = document.querySelector("#messageInput"),
-    sendMsgButton = ducment.querySelector("sendMsgButton");
+
+const userName = document.querySelector("#userName");
+const messages = document.querySelector("#messages");
+const messageInput = document.querySelector("#messageInput");
+const sendMsgButton = document.querySelector("#sendMessagesButton");
 
 Swal.fire({
-    title: "Ingresa tu nombre:",
-    input: "text",    
-
-    inputValidator: (value) => {
-        if (!value) {
-            return "Error. Nombre obligatorio!"
-        }
-    },
+  title: "Ingresa tu nombre:",
+  input: "text",
+  inputValidator: (value) => {
+    if (!value) return "Error. Nombre obligatorio!";
+  },
 }).then((result) => {
-    user = result.value;
-    userName.innerText = user;
+  user = result.value;
+  userName.innerText = user;
 });
 
 sendMsgButton.addEventListener("click", () => {
-    if (messageInput.value.trim().lenght > 0) {
-        socket.emit("message", {user, message: messageInput.value });
-        messageInput.value = "";
-    }
+  if (messageInput.value.trim().length > 0) {
+    socket.emit("message", {
+      user,
+      message: messageInput.value,
+    });
+    messageInput.value = "";
+  }
 });
 
 socket.on("messages-logs", (data) => {
-    let listMessages = "";
+  messages.innerHTML = "";
+  data.forEach((msg) => {
     const p = document.createElement("p");
-    data.forEach((msg) => {
-        listMessages = `${msg.user} dice : ${msg.message}`;
-        p.innerHTML = listMessages;
-    });
-    messages.appendChild(p)
-})
+    p.innerText = `${msg.user} dice: ${msg.message}`;
+    messages.appendChild(p);
+  });
+});
